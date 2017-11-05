b7c07186041347ecf39afb325cdfaf3e197f92d6
Monika <arschlein@seznam.cz>
2017-11-05 11:52:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value){
 //object[key] = value;
 //return object;
-  var newObject = Object.assign({}, object, {key: value})
+  var newObject = object.assign({}, object, {key: value})
   return newObject;
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
   return object;
+}
+function deleteFromObjectByKey(object, key){
+  var newObject = object.assign({}, object);
+  delete newObject[key];
+  return newObject;
 }
