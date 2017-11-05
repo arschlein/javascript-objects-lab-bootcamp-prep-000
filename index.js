83e6f2e19713ea8b8b3a4e56c6e26770e2d48282
Monika <arschlein@seznam.cz>
2017-11-05 12:00:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value){
 //object[key] = value;
 //return object;
-  var newObject = Object.assign({}, object, {key: value})
+  var newObject = Object.assign({}, object)
   newObject[key]=value;
   return newObject;
 }
